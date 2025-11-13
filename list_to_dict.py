641ac0911b14bcfcb650ea62da927ad99ccdecc3
logan <blacksuitphoto@gmail.com>
2025-11-13 12:47:20 -0800

    completed function

--- list_to_dict.py
+++ list_to_dict.py
@@ -2,11 +2,21 @@
 
 list1 = ['table', 'chair', 'rack', 'shelf']
 list2 = [100,50,80,40]
-combined_lists = {}
 
 def list_to_dict(list1, list2):
-
+    combined_lists = {}
     for item in list1:
-        combined_lists.append(item)
-    print(combined_lists)
+        combined_lists[item] = ''
+
+    count = 0
+        
+    for key in combined_lists:
+        combined_lists[key] = list2[count]
+        count += 1
+    return combined_lists
+
+print(list_to_dict(list1, list2))
+
+
+
 
